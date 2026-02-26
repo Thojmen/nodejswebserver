697771c5c5b48f43bee135649c023ebae5d61078
Thojmen <thijmenvanbreugel@gmail.com>
2026-02-26 15:03:50 +0100

    pretty logs

--- webserver/hello-world.js
+++ webserver/hello-world.js
@@ -1,14 +1,17 @@
 const http = require('node:http');
+const pretty = require('pretty-log');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
+  pretty.info(`Nieuwe request: ${req.method} ${req.url}`);
+
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World');
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  pretty.success(`Server gestart op http://${hostname}:${port}/`);
 });
